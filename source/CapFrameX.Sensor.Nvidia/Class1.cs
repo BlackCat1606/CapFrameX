95e334e47f9ce461df6ea30d5ebcba853f726c00
DevTechProfile <devtechprofile@hotmail.com>
2020-02-24 21:19:31 +0100

    initial sensor data project structure

---
+++ source/CapFrameX.Sensor.Nvidia/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapFrameX.Sensor.Nvidia
+{
+    public class Class1
+    {
+    }
+}
